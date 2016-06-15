39d30372b76bdd27cba5bad5ae74e5c18bcaa2c3
mw12345 <maciej.wojton@gmail.com>
2016-06-14 22:27:23 -0400

    update

---
+++ render.js
@@ -0,0 +1,8 @@
+var page = require('webpage').create();
+page.open('vim_cheatsheet.html', function(status) {
+  console.log("Status: " + status);
+  if(status === "success") {
+    page.render('vim_cheatsheet.png');
+  }
+  phantom.exit();
+});
