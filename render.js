80c5e840170215b5b17d13961d85d2b1f2da72c9
mw12345 <maciej.wojton@gmail.com>
2016-08-08 12:23:59 -0400

    pdf

--- render.js
+++ render.js
@@ -1,8 +1,11 @@
 var page = require('webpage').create();
 page.open('vim_cheatsheet.html', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    page.render('vim_cheatsheet.png');
-  }
-  phantom.exit();
+    
+    console.log("Status: " + status);
+    if(status === "success") {
+        page.render('vim_cheatsheet.pdf');
+        page.zoomFactor = 2;
+        page.render('vim_cheatsheet.png');
+    }
+    phantom.exit();
 });
