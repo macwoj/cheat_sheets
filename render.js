61f98c7cec81676e0ffca4d88a15b9da1bd45fa8
mw12345 <maciej.wojton@gmail.com>
2016-12-13 21:33:15 -0500

    added png

--- render.js
+++ render.js
@@ -9,10 +9,35 @@ var saveTable = function() {
         console.log("Status: " + status);
         if(status === "success") {
             page.render('vim_cheatsheet_table.pdf');
+            saveTablePng();
+        }
+    });
+}  
+
+var saveTablePng = function() {
+    var page = require('webpage').create();
+    page.viewportSize = { width: 1920*.75, height: 1080*.8 };
+    page.open('vim_cheatsheet_table.html', function(status) {
+        console.log("Status: " + status);
+        if(status === "success") {
+            page.render('vim_cheatsheet_table.png');
             phantom.exit();
         }
     });
 }
+
+var savePng = function() {
+    var page = require('webpage').create();
+    page.viewportSize = { width: 1920*.75, height: 1080*.8 };
+    page.open('vim_cheatsheet.html', function(status) {
+        console.log("Status: " + status);
+        if(status === "success") {
+            page.render('vim_cheatsheet.png');
+            saveTable();
+        }
+    });
+} 
+ 
 var save = function() {
     var page = require('webpage').create();
     page.paperSize = {
@@ -24,7 +49,7 @@ var save = function() {
         console.log("Status: " + status);
         if(status === "success") {
             page.render('vim_cheatsheet.pdf');
-            saveTable();
+            savePng();
         }
     });
 }
